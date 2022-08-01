d5f0761b9f80205549fc0231b635e72e836356ec
Stephen Smith <StephenASmith7178@gmail.com>
2022-08-01 11:48:08 -0400

    exerciseView displaying

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -19,7 +19,6 @@ export const Login = () => {
                         id: customer.id,
                         email: customer.email
                     }))
-
                     navigate("/profile/:customerId")
                 }
                 else {
--- src/components/exercise/ExerciseView.js
+++ src/components/exercise/ExerciseView.js
@@ -1,67 +1,40 @@
-// import { click } from "@testing-library/user-event/dist/click"
 import { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
+import { Button } from "reactstrap"
 import "./Exercise.css"
 
 export const ExerciseView = () => {
     const { workoutId } = useParams()
     const [workout, setWorkout] = useState({})
-    // const shownWorkout = Number(workoutId)
-    
+
     useEffect(
         () => {
             fetch(`http://localhost:8088/workoutExercises?id=${workoutId}`)
-            .then(response => response.json())
-            .then((data) => {
-                const singleWorkout = data[0]
-                setWorkout(singleWorkout)
-            })
+                .then(response => response.json())
+                .then((data) => {
+                    const singleWorkout = data[0]
+                    setWorkout(singleWorkout)
+                })
         },
         [workoutId] // When this array is empty, you are observing initial component state
-        )
-        // const getExercises = () => {
-        //  return workout.exercises.map(exercise => ({ ...exercise }))
-        // }
-        // const exercises = getExercises()
-    //export function of each array mapped to access each object  key:values  
-       const thisWorkout = () => {
-          console.log(workout)
-          let str = null
-         str = JSON.stringify(workout.exercises)
-        console.log(str)
-        //str.forEach(() => (exercise){ $('tbody').append('<tr>,<td>'+ exercise.name + '</td><td>'
-        //+ exercise["sets"]+'</td><td>' + exercise.reps+'</td></tr>')
+    )
 
-        //})
+    const thisWorkout = () => {
+        return workout?.exercises?.map((exercise) => {
+            return <ul>
+                <li key="exercise--{exercise.id}">
+                    <strong>{exercise.name}</strong>&nbsp;<br />
+                    sets:&nbsp;{exercise.sets}&nbsp;&nbsp;
+                    reps:&nbsp;{exercise.reps}&nbsp;&nbsp;
+                    rest time:&nbsp;{exercise.rest}<br />
+                    <a href={exercise.exampleVid}><Button className="exercise__link"
+                    >Watch tutorial</Button></a>
+                </li>
+            </ul>
         }
-    //             exercises.map((exercise) => {
-    //                 return <ul>
-    //                     <li key="exercise--{exercise.id}">
-    //                         <strong>{exercise.name}</strong>&nbsp;<br />
-    //                         sets:&nbsp;{exercise.sets}&nbsp;&nbsp;
-    //                         reps:&nbsp;{exercise.reps}&nbsp;&nbsp;
-    //                         rest time:&nbsp;{exercise.rest}<br />
-    //                         <button className="exercise__link"
-    //                         >Watch tutorial</button>
-    //                     </li>
-    //                 </ul>
-    //             })}  
-            
-    // const workoutName = () => {
-          
-    //             <>{workout.exercises.map((exercise) => {
-    //                 return <ul>
-    //                     <li key="exercise--{exercise.id}">
-    //                         <strong>{exercise.name}</strong>&nbsp;<br />
-    //                         sets:&nbsp;{exercise.sets}&nbsp;&nbsp;
-    //                         reps:&nbsp;{exercise.reps}&nbsp;&nbsp;
-    //                         rest time:&nbsp;{exercise.rest}<br />
-    //                         <button className="exercise__link"
-    //                         >Watch tutorial</button>
-    //                     </li>
-    //                 </ul>
-    //             })}  </>
-    //         }
+        )
+    }
+
     return (
         <>
             <div className='generator__nav'>
@@ -81,29 +54,7 @@ export const ExerciseView = () => {
             <>
                 <h2 className="workout"><b>MyWorkout</b></h2>
                 <div className="workout__exercises">
-{/* 
-                    {workoutName()} */}
                     {thisWorkout()}
-                    
-{/* 
-                    <div className="workout__exercises">
-                        <ul>
-                            {workoutExercises.map(workout => {
-                                if (workout.id === shownWorkout) {
-                                    workout.exercises.map((exercise) => {
-                                        return <li className="w__e" key="{exercise.id}">
-                                            <strong>{exercise.name}</strong>&nbsp;<br />
-                                            sets:&nbsp;{exercise.sets}&nbsp;&nbsp;
-                                            reps:&nbsp;{exercise.reps}&nbsp;&nbsp;
-                                            rest time:&nbsp;{exercise.rest}<br />
-                                            <button className="exercise__link"
-                                            >Watch tutorial</button>
-                                        </li>
-                                    })
-                                }
-                            })}
-                        </ul>
-                    </div> */}
                 </div></>
         </>
     )
