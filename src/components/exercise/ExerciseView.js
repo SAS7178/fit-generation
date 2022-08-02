6a19ae12b821c031fb747e9298fafef52f3d9ea2
Stephen Smith <StephenASmith7178@gmail.com>
2022-08-01 21:06:43 -0400

    exerciseView

--- src/components/exercise/ExerciseView.js
+++ src/components/exercise/ExerciseView.js
@@ -5,20 +5,10 @@ import "./Exercise.css"
 
 export const ExerciseView = () => {
     const { workoutId } = useParams()
-    const [workout, setWorkout] = useState({})
+    // const [workouts, setWorkouts] = useState([])
     const [exercises, setExercises] = useState([])
     const [workoutExercises, setWorkoutExercises] = useState([])
-
-    useEffect(
-        () => {
-            fetch(`http://localhost:8088/workouts?id=${workoutId}`)
-                .then(response => response.json())
-                .then((data) => {
-                    setWorkout(data)
-                })
-        },
-        [workoutId] // When this array is empty, you are observing initial component state
-    )
+    const workoutid = Number(workoutId)
 
     useEffect(
         () => {
@@ -41,19 +31,21 @@ export const ExerciseView = () => {
         [] // When this array is empty, you are observing initial component state
     )
 
+    //return array of filterd workout exercise objects 
     const filteredWorkoutExercises = () => {
-        let filtered = []
+        const filteredWExercises = []
         workoutExercises.map((workoutExercise) => {
-            if (workout.id === workoutExercise.workoutId) {
-                filtered.push(workoutExercise)
-            } return filtered
+            if (workoutid === workoutExercise.workoutId) {
+                filteredWExercises.push(workoutExercise)
+            } return filteredWExercises
         })
     }
-
-    const displayExercises = (filteredWorkoutExercises) => {
-        exercises.map(exercise => {
-            filteredWorkoutExercises.map(workoutExercise => {
-                if (exercise.id === workoutExercise.exerciseId) {
+    const filteredWExercises = filteredWorkoutExercises()
+    //if filterd exercise objects.exerciseId and exercises have same id list each exercises info to display
+    const displayExercises = (filteredWExercises) => {
+        for (const filteredWExercise of filteredWExercises) {
+            for (const exercise of exercises) {
+                if (filteredWExercise.exerciseId === exercise.id) {
                     return <ul>
                         <li key="exercise--{exercise.id}">
                             <strong>{exercise.name}</strong>&nbsp;<br />
@@ -65,13 +57,9 @@ export const ExerciseView = () => {
                         </li>
                     </ul>
                 }
-            })
-        })
+            }
+        }
     }
-      const filters = filteredWorkoutExercises()
-         
-    
-
 
     return (
         <>
@@ -85,15 +73,15 @@ export const ExerciseView = () => {
             </div>
             <div className='welcome__header'>
                 <h1> Fit Generation </h1>
-                <img className='nav__image' src="https://ae01.alicdn.com/kf/HTB1e2SGSbvpK1RjSZFqq6AXUVXax/Gym-fitness-
+                <img className='nav__image' alt="mehhh" src="https://ae01.alicdn.com/kf/HTB1e2SGSbvpK1RjSZFqq6AXUVXax/Gym-fitness-
             exercise-metal-Cutting-Dies-Scrapbooking-craft-Dies-cuts-thin-paper-emboss-
             card-make-stencil.jpg_640x640.jpg" width="100" height="100"></img>
             </div>
             <>
                 <h2 className="workout"><b>MyWorkout</b></h2>
                 <div className="workout__exercises">
-                {displayExercises(filters)}
-                      </div></>
+                    {displayExercises(filteredWExercises)}
+                </div></>
         </>
     )
 }
