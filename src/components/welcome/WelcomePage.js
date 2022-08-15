5ab96c80c03383f092cd47cee9f4237a95d190f6
Stephen Smith <StephenASmith7178@gmail.com>
2022-08-15 08:45:06 -0400

    nonmember homepage style and funtionality

--- src/components/welcome/WelcomePage.js
+++ src/components/welcome/WelcomePage.js
@@ -12,77 +12,135 @@ export const WelcomePage = () => {
   const handleGenerateButtonClick = () => {
     navigate(`/generateWorkout`)
   }
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'df702358e3msh0a60399ba97f41ap1be78cjsn2053bd221176',
-      'X-RapidAPI-Host': 'bodybuilding-quotes1.p.rapidapi.com'
-    }
-  };
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'df702358e3msh0a60399ba97f41ap1be78cjsn2053bd221176',
+  //     'X-RapidAPI-Host': 'bodybuilding-quotes1.p.rapidapi.com'
+  //   }
+  // };
 
-  useEffect(
-    () => {
-     fetch('https://bodybuilding-quotes1.p.rapidapi.com/random-quote', options)
-       .then(response => response.json())
-           .then(response => {setQoute(response)
-        })
-       },
-    [] // When this array is empty, you are observing initial component state
-  )
+  // useEffect(
+  //   () => {
+  //    fetch('https://bodybuilding-quotes1.p.rapidapi.com/random-quote', options)
+  //      .then(response => response.json())
+  //          .then(response => {setQoute(response)
+  //       })
+  //      },
+  //   [] // When this array is empty, you are observing initial component state
+  // )
 
-  return (
-    <div className="">
-      <section className="card-box">
-        <Card inverse className="welcome__card">
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/900/270?grayscale"
-            className="card-img"
-          />
-          <CardImgOverlay className="overLay">
-            <CardTitle className="qoute-box" tag="h5">
-              "{`${quote.quote}`}"
-              -{`${quote.author}`}-
-            </CardTitle>
-            <CardText>
-              <small className="text-quote">
-              </small>
-            </CardText>
-          </CardImgOverlay>
-        </Card>
-      </section>
-      <div className="seperation"></div>
-      <iframe className="welcomeIframe" width="460" height="350" src="https://www.youtube.com/embed/Kpbo9SXbGXE" 
-      title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+  //get fitCustomer from local set to var
+  const localFitCustomer = localStorage.getItem("fit_customer")
+  const fitCustomerObject = JSON.parse(localFitCustomer)
+  // return one nav if customer true return other if false
+  if (fitCustomerObject) {
+    return (
+      <div className="">
+        <section className="card-box">
+          <Card inverse className="welcome__card">
+            <CardImg
+              alt="Card image cap"
+              src="https://picsum.photos/900/270?grayscale"
+              className="card-img"
+            />
+            <CardImgOverlay className="overLay">
+              <CardTitle className="qoute-box" tag="h5">
+                "{`${quote.quote}`}"
+                -{`${quote.author}`}-
+              </CardTitle>
+              <CardText>
+                <small className="text-quote">
+                </small>
+              </CardText>
+            </CardImgOverlay>
+          </Card>
+        </section>
+        <div className="seperation"></div>
+        <iframe className="welcomeIframe" width="460" height="350" src="https://www.youtube.com/embed/Kpbo9SXbGXE"
+          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
       encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <section className="tagline" >
-        <div className="tag-logo">
-          <b>Where <strong>Fitness Generation</strong> is our Mission!</b>
+        {/* <iframe className="welcomeIframe" width="560" height="315" src="https://www.youtube.com/embed/BHY0FxzoKZE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+        <section className="tagline" >
+          <div className="tag-logo">
+            <b>Where <strong>Fitness Generation</strong> is our Mission!</b>
 
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <div className="welcome-btns">
-          <button
-            onClick={(evt) => handleGenerateButtonClick(evt)}
-            className="btn-generate">
-            Generate Workout
-          </button>
+          </div>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button
-            onClick={() => { window.open(URL = "https://whatnerd.com/apps-gamify-fitness-and-health/") }}
-            className="btn-games">
-            Exercise Games
-          </button>
-          <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png"  width="50%" height="100em"></img>
-          <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="90%" height="100em"></img>
-        </div>
-        <img alt="" className='gorilla-home' src="/2E5049CB-BAED-4DAC-97C5-065C0E195D5E_4_5005_c.jpeg" width="200" height="200" ></img>
-        <div className="lin">
-        </div>
-      </section>
-      <WelcomeFooter />
-    </div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <div className="welcome-btns">
+            <button
+              onClick={(evt) => handleGenerateButtonClick(evt)}
+              className="btn-generate">
+              Generate Workout
+            </button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              onClick={() => { window.open(URL = "https://whatnerd.com/apps-gamify-fitness-and-health/") }}
+              className="btn-games">
+              Exercise Games
+            </button>
+            <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="50%" height="100em"></img>
+            <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="90%" height="100em"></img>
+          </div>
+          <img alt="" className='gorilla-home' src="/2E5049CB-BAED-4DAC-97C5-065C0E195D5E_4_5005_c.jpeg" width="200" height="200" ></img>
+          <div className="lin">
+          </div>
+        </section>
+        <WelcomeFooter />
+      </div>
 
-  )
-}
+    )
+  } else {
+    return (
+      <div className="">
+        <section className="card-box">
+          <Card inverse className="welcome__card">
+            <CardImg
+              alt="Card image cap"
+              src="https://picsum.photos/900/270?grayscale"
+              className="card-img"
+            />
+            <CardImgOverlay className="overLay">
+              <CardTitle className="qoute-box" tag="h5">
+                "{`${quote.quote}`}"
+                -{`${quote.author}`}-
+              </CardTitle>
+              <CardText>
+                <small className="text-quote">
+                </small>
+              </CardText>
+            </CardImgOverlay>
+          </Card>
+        </section>
+        <div className="seperation"></div>
+        <div className="taglinenon"><b>Where <strong>Fitness Generation</strong> is our Mission!</b></div>
+        <div className="nonVids">
+        <iframe className="welcomeTTIframe" width="560" height="315" src="https://www.youtube.com/embed/BHY0FxzoKZE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className="welcomeIframe" width="460" height="350" src="https://www.youtube.com/embed/Kpbo9SXbGXE"
+          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+    encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <section className="taglinenonb" >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <div className="welcome-btns">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              onClick={() => { window.open(URL = "https://whatnerd.com/apps-gamify-fitness-and-health/") }}
+              className="btn-games">
+              Exercise Games
+            </button>
+          </div>
+          <img alt="" className='gorilla-home' src="/2E5049CB-BAED-4DAC-97C5-065C0E195D5E_4_5005_c.jpeg" width="200" height="200" ></img>
+          </section>
+         
+       
+        <WelcomeFooter />
+      </div>
+    )
+  }
+} 
